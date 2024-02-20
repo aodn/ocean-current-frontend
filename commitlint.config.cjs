121bf2f2b7ef69c8cfca04d3be4660ce549291a3
German R <germanrodrickson@gmail.com>
2024-02-20 16:01:48 +1100

    :tada: start ocean current project

---
+++ commitlint.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['gitmoji'],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'type-enum': [0],
+    'subject-empty': [0],
+    'type-empty': [0],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="underline text-3xl text-center">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
