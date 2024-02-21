46030da881b6bdfa2673836f56118680e18e2f54
German R <germanrodrickson@gmail.com>
2024-02-21 13:53:21 +1100

    :white_check_mark: add basic button component with test

---
+++ src/components/shared/button/Button.test.tsx
@@ -0,0 +1,68 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Button, { BorderRadius } from './Button';
+
+describe('Button Component', () => {
+  it('renders without crashing', () => {
+    render(
+      <Button
+        color="text-white"
+        borderColor="border-red-500"
+        backgroundColor="bg-blue-500"
+        borderRadius={BorderRadius.Medium}
+      >
+        Click me
+      </Button>,
+    );
+    expect(screen.getByText('Click me')).toBeInTheDocument();
+  });
+
+  it('renders the icon when hasIcon is true', () => {
+    const icon = <span>🚀</span>;
+    render(
+      <Button
+        color="text-white"
+        borderColor="border-red-500"
+        backgroundColor="bg-blue-500"
+        borderRadius={BorderRadius.Medium}
+        hasIcon={true}
+        icon={icon}
+      >
+        Launch
+      </Button>,
+    );
+    expect(screen.getByText('🚀')).toBeInTheDocument();
+  });
+
+  it('applies styles based on props', () => {
+    const { container } = render(
+      <Button
+        color="text-white"
+        borderColor="border-red-500"
+        backgroundColor="bg-blue-500"
+        borderRadius={BorderRadius.Medium}
+      >
+        Styled Button
+      </Button>,
+    );
+    expect(container.firstChild).toHaveClass('text-white border-red-500 bg-blue-500 rounded-md');
+  });
+
+  it('calls onClick when button is clicked', () => {
+    const onClickMock = vi.fn();
+    render(
+      <Button
+        color="text-white"
+        borderColor="border-red-500"
+        backgroundColor="bg-blue-500"
+        borderRadius={BorderRadius.Medium}
+        onClick={onClickMock}
+      >
+        Click me
+      </Button>,
+    );
+    fireEvent.click(screen.getByText('Click me'));
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
