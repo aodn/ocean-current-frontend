d9a792bd69e94f4167c1ff6b6c505f47bde3be0f
German R <germanrodrickson@gmail.com>
2024-02-28 11:48:59 +1100

    :white_check_mark: add basic test for navbar

---
+++ src/components/Navbar/Navbar.test.tsx
@@ -0,0 +1,33 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { render, fireEvent, RenderResult } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Navbar from './Navbar';
+import NavbarMenu from './components/NavbarMenu';
+
+describe('Navbar component', () => {
+  let component: RenderResult;
+
+  beforeEach(() => {
+    component = render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>,
+    );
+  });
+
+  it('Navbar renders without crashing', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('NavbarMenu renders without crashing', () => {
+    const { getByText } = render(<NavbarMenu itemsLeft={[]} itemsRight={[]} />);
+    expect(getByText).toBeTruthy();
+  });
+
+  it('updates hoverIndex and popoverPosition on mouse enter', async () => {
+    const { findAllByText, findByText } = component;
+    const menuItem = await findAllByText('Maps');
+    fireEvent.mouseEnter(menuItem[0]);
+    expect(await findByText('Snapshot SST')).toBeInTheDocument();
+  });
+});
