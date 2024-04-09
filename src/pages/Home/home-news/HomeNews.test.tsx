82eccc7ffa8f44cd541ca9224ae3dddad7c7e647
German R <germanrodrickson@gmail.com>
2024-04-09 11:21:10 +1000

    :white_check_mark: update home news test

--- src/pages/Home/home-news/HomeNews.test.tsx
+++ src/pages/Home/home-news/HomeNews.test.tsx
@@ -1,74 +1,70 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import DateTitleContent from './components/CardLayout/DateTitleContent';
-import Horizontal from './components/CardLayout/Horizontal';
-import HomeNews from './HomeNews';
+import NewsCard from './components/NewsCard/NewsCard';
 import { News } from './types/HomeNews.types';
 
 const mockNews: News[] = [
   {
     id: 1,
     title: 'Test News Title 1',
-    author: 'John Doe',
+    author: 'John Doe 1',
     imageUrl: 'https://oceancurrent.aodn.org.au/news/20240202/Bonney1.png',
-    content: 'Late last year we took note of the persistent upwelling along the Bonney Coast.',
-    date: '2023-04-01T12:00:00Z',
+    content: 'Late last year we took note of the persistent upwelling along the Bonney Coast 1',
+    date: '2023-04-01T12:00:00Z 1',
   },
   {
     id: 2,
     title: 'Test News Title 2',
-    author: 'John Doe',
-    imageUrl: 'https://oceancurrent.aodn.org.au/news/20240202/Bonney1.png',
-    content: 'Late last year we took note of the persistent upwelling along the Bonney Coast.',
-    date: '2023-04-01T12:00:00Z',
+    author: 'John Doe 2',
+    imageUrl: 'https://oceancurrent.aodn.org.au/news/20240202/Bonney2.png',
+    content: 'Late last year we took note of the persistent upwelling along the Bonney Coast 2',
+    date: '2023-04-01T12:00:00Z 2',
   },
 ];
 
-describe('Home news section', () => {
-  describe('DateTitleContent Component', () => {
-    it('renders component correctly with given news data', () => {
-      // Arrange
-      render(<DateTitleContent news={mockNews[0]} />);
+describe('NewsCard Component', () => {
+  it('renders DateTitleContentLayout by default', () => {
+    // Arrange
+    render(<NewsCard news={mockNews[0]} />);
 
-      // Act and Assert
-      expect(screen.getByTestId('news-title-card')).toHaveTextContent('Test News Title 1');
-      expect(screen.getByTestId('news-date-card')).toHaveTextContent(/2023-04-01/);
-      expect(screen.getByTestId('news-author-card')).toHaveTextContent('John Doe');
-      expect(screen.getByTestId('news-content-card')).toHaveTextContent(
-        'Late last year we took note of the persistent upwelling along the Bonney Coast.',
-      );
-    });
+    // Act and Assert
+    expect(screen.getByTestId('news-title-card')).toHaveTextContent('Test News Title 1');
+    expect(screen.getByTestId('news-date-card')).toHaveTextContent(/2023-04-01/);
+    expect(screen.getByTestId('news-author-card')).toHaveTextContent('John Doe 1');
+    expect(screen.getByTestId('news-content-card')).toHaveTextContent(
+      'Late last year we took note of the persistent upwelling along the Bonney Coast 1',
+    );
   });
 
-  describe('Horizontal Component', () => {
-    it('renders component correctly with given news data', () => {
-      // Arrange
-      render(<Horizontal news={mockNews[1]} />);
+  it('renders DateTitleContentLayout when layout prop is "date-title-content"', () => {
+    // Arrange
+    render(<NewsCard news={mockNews[0]} layout="date-title-content" />);
 
-      // Act and Assert
-      expect(screen.getByTestId('news-title-card')).toHaveTextContent('Test News Title 2');
-      expect(screen.getByTestId('news-content-card')).toHaveTextContent(
-        'Late last year we took note of the persistent upwelling along the Bonney Coast.',
-      );
-      expect(screen.getByTestId('news-img-card')).toHaveAttribute(
-        'src',
-        'https://oceancurrent.aodn.org.au/news/20240202/Bonney1.png',
-      );
-      expect(screen.getByTestId('news-img-card')).toHaveAttribute('alt', 'Test News Title 2 news img');
-      expect(screen.getByTestId('news-button-card')).toHaveTextContent('Details');
-    });
+    // Act and Assert
+    expect(screen.getByTestId('news-title-card')).toHaveTextContent('Test News Title 1');
+    expect(screen.getByTestId('news-date-card')).toHaveTextContent(/2023-04-01/);
+    expect(screen.getByTestId('news-author-card')).toHaveTextContent('John Doe');
+    expect(screen.getByTestId('news-content-card')).toHaveTextContent(
+      'Late last year we took note of the persistent upwelling along the Bonney Coast 1',
+    );
   });
 
-  describe('HomeNews Component', () => {
-    it('renders component the news section with news cards', () => {
-      // Arrange
-      render(<HomeNews />);
+  it('renders HorizontalLayout when layout prop is "horizontal"', () => {
+    // Arrange
+    render(<NewsCard news={mockNews[1]} layout="horizontal" />);
 
-      // Act and Assert
-      expect(screen.getByText('OceanCurrent News')).toBeInTheDocument();
-      expect(screen.getByText('A Bonney Bloom')).toBeInTheDocument();
-      expect(screen.getByText('Sydney to Hobart race conditions: exceptional currents')).toBeInTheDocument();
-    });
+    // Act and Assert
+    expect(screen.queryByTestId('news-date-card')).not.toBeInTheDocument();
+    expect(screen.getByTestId('news-title-card')).toHaveTextContent('Test News Title 2');
+    expect(screen.getByTestId('news-content-card')).toHaveTextContent(
+      'Late last year we took note of the persistent upwelling along the Bonney Coast 2',
+    );
+    expect(screen.getByTestId('news-img-card')).toHaveAttribute(
+      'src',
+      'https://oceancurrent.aodn.org.au/news/20240202/Bonney2.png',
+    );
+    expect(screen.getByTestId('news-img-card')).toHaveAttribute('alt', 'Test News Title 2 news img');
+    expect(screen.getByTestId('news-button-card')).toHaveTextContent('Details');
   });
 });
