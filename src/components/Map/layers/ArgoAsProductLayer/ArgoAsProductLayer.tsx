2097d218db71cc9b2f981109d0e4c33bbafcba0f
Weite Dai <weite.dai@utas.edu.au>
2024-05-01 15:42:39 +1000

    :sparkles: show argo info popup when hover circle

--- src/components/Map/layers/ArgoAsProductLayer/ArgoAsProductLayer.tsx
+++ src/components/Map/layers/ArgoAsProductLayer/ArgoAsProductLayer.tsx
@@ -1,8 +1,9 @@
-import { Layer, MapMouseEvent, Source, useMap } from 'react-map-gl';
-import { useEffect, useRef } from 'react';
+import { Layer, MapMouseEvent, Popup, Source, useMap } from 'react-map-gl';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useArgoStore from '@/stores/argo-store/argoStore';
 import { mapboxLayerIds, mapboxSourceIds } from '@/constants/mapboxId';
+import { ArgoProfile } from '@/types/argo';
 import useArgoAsProductData from '../../hooks/useArgoAsProductData';
 
 const ArgoAsProductLayer = () => {
@@ -13,6 +14,8 @@ const ArgoAsProductLayer = () => {
   const { current: map } = useMap();
   const navigate = useNavigate();
   const eventAdded = useRef(false);
+
+  const [popupInfo, setPopupInfo] = useState<ArgoProfile | null>();
 
   useEffect(() => {
     if (!map) return;
@@ -31,50 +34,84 @@ const ArgoAsProductLayer = () => {
       navigate(clickedArgoPath);
     };
 
-    map.on('load', () => {
-      if (!eventAdded.current) {
-        eventAdded.current = true;
-        map.on('click', argoAsProductPointLayer, handleClick);
-        map.on('mouseenter', argoAsProductPointLayer, () => {
-          map.getCanvas().style.cursor = 'pointer';
-        });
-        map.on('mouseleave', argoAsProductPointLayer, () => {
-          map.getCanvas().style.cursor = '';
-        });
-      }
-    });
+    if (!eventAdded.current) {
+      eventAdded.current = true;
+      map.on('click', argoAsProductPointLayer, handleClick);
+      map.on('mouseenter', argoAsProductPointLayer, (e) => {
+        map.getCanvas().style.cursor = 'pointer';
+        const features = map.queryRenderedFeatures(e.point, { layers: [argoAsProductPointLayer] });
+        if (!features.length) {
+          return;
+        }
+        if (features[0].geometry.type === 'Point') {
+          const clickedArgoParam = features[0].properties;
+          const clickedArgoCoords = features[0].geometry.coordinates;
+          if (!clickedArgoParam) {
+            return;
+          }
+          const { worldMeteorologicalOrgId, cycle, depth, date } = clickedArgoParam;
+          setPopupInfo({
+            worldMeteorologicalOrgId,
+            cycle,
+            depth,
+            date,
+            coords: clickedArgoCoords,
+          });
+        }
+      });
+      map.on('mouseleave', argoAsProductPointLayer, () => {
+        map.getCanvas().style.cursor = '';
+        setPopupInfo(null);
+      });
+    }
+
     return () => {
-      map.off('click', argoAsProductPointLayer, handleClick);
+      if (map && eventAdded.current) {
+        map.off('click', argoAsProductPointLayer, handleClick);
+        eventAdded.current = false;
+      }
     };
   }, [argoAsProductPointLayer, map, navigate]);
 
   return (
-    <Source id={argoAsProductSource} type="geojson" data={argoData}>
-      <Layer
-        id={argoAsProductSelectedPointLayer}
-        type="circle"
-        source={argoAsProductSource}
-        paint={{
-          'circle-radius': 15,
-          'circle-color': 'white',
-          'circle-opacity': 0.5,
-          'circle-stroke-width': 1,
-          'circle-stroke-color': 'white',
-        }}
-        filter={['==', 'worldMeteorologicalOrgId', selectedWorldMeteorologicalOrgId]}
-      />
-      <Layer
-        id={argoAsProductPointLayer}
-        type="circle"
-        source={argoAsProductSource}
-        paint={{
-          'circle-radius': 8,
-          'circle-color': '#524DAB',
-          'circle-stroke-color': 'transparent',
-          'circle-stroke-width': 15,
-        }}
-      />
-    </Source>
+    <>
+      {popupInfo && (
+        <Popup
+          closeButton={false}
+          latitude={popupInfo.coords[1]}
+          longitude={popupInfo.coords[0]}
+          offset={[0, -24] as [number, number]}
+        >
+          <div>wmoid: {popupInfo.worldMeteorologicalOrgId}</div>
+        </Popup>
+      )}
+      <Source id={argoAsProductSource} type="geojson" data={argoData}>
+        <Layer
+          id={argoAsProductSelectedPointLayer}
+          type="circle"
+          source={argoAsProductSource}
+          paint={{
+            'circle-radius': 15,
+            'circle-color': 'white',
+            'circle-opacity': 0.5,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': 'white',
+          }}
+          filter={['==', 'worldMeteorologicalOrgId', selectedWorldMeteorologicalOrgId]}
+        />
+        <Layer
+          id={argoAsProductPointLayer}
+          type="circle"
+          source={argoAsProductSource}
+          paint={{
+            'circle-radius': 8,
+            'circle-color': '#524DAB',
+            'circle-stroke-color': 'transparent',
+            'circle-stroke-width': 15,
+          }}
+        />
+      </Source>
+    </>
   );
 };
 
--- src/pages/Home/product-carrousel/ProductCarrousel.tsx
+++ src/pages/Home/product-carrousel/ProductCarrousel.tsx
@@ -1,21 +1,45 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { setProductKey } from '@/stores/product-store/productStore';
+import useMapStore from '@/stores/map-store/mapStore';
 import { productsData } from './data/ProductsData';
 import ProductCarrouselCard from './components/ProductCarrouselCard';
 
 const ProductCarrousel: React.FC = () => {
   const [selectedProductIndex, setSelectedProductIndex] = useState<number>(0);
-
   const selectedProduct = productsData[selectedProductIndex];
+
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const isMounted = useRef<boolean>(false);
+
+  const useZoom = useMapStore((state) => state.mapViewState.zoom);
+
+  const stopInterval = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+  };
+
+  const handleClick = (id: string) => {
+    stopInterval();
+    const foundIndexById = productsData.findIndex((product) => product.id === id);
+    setSelectedProductIndex(foundIndexById);
+  };
+
+  useEffect(() => {
+    if (isMounted.current) {
+      stopInterval();
+    } else {
+      isMounted.current = true;
+    }
+  }, [useZoom]);
 
   useEffect(() => {
     const switchSelectedProduct = () => {
       setSelectedProductIndex((preSelectedIndex) => (preSelectedIndex + 1) % productsData.length);
     };
+    intervalRef.current = setInterval(switchSelectedProduct, 2500);
 
-    const intervalIndex = setInterval(switchSelectedProduct, 2500);
-
-    return () => clearInterval(intervalIndex);
+    return () => stopInterval();
   }, []);
 
   useEffect(() => {
@@ -25,7 +49,7 @@ const ProductCarrousel: React.FC = () => {
   return (
     <ul>
       {productsData.map((product) => (
-        <li className="[&:not(:last-child)]:mb-5" key={product.id}>
+        <li className="[&:not(:last-child)]:mb-5" key={product.id} onClick={() => handleClick(product.id)} aria-hidden>
           <ProductCarrouselCard
             title={product.title}
             description={product.description}
