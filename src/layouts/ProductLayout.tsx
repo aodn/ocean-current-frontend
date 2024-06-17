7a8563555cabea47c2061264bb78b96332390663
GermanRodrickson <germanrodrickson@gmail.com>
2024-06-17 18:24:22 +1000

    :sparkles: add button to hide sidebar

--- src/layouts/ProductLayout.tsx
+++ src/layouts/ProductLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Outlet, useSearchParams } from 'react-router-dom';
 import dayjs from 'dayjs';
 import { setArgoData } from '@/stores/argo-store/argoStore';
@@ -11,12 +11,16 @@ import { useProductFromUrl, useProductSearchParam } from '@/hooks';
 import { getRegionByRegionTitle } from '@/utils/region';
 import ErrorBoundary from '@/errors/error-boundary/ErrorBoundary';
 import NewMapNavbar from '@/components/ProductNavbar/ProductNavbar';
+import ArrowIcon from '@/assets/icons/arrow.svg';
 
 const ProductLayout: React.FC = () => {
   const [searchParams] = useSearchParams();
   const { isArgo } = useProductCheck();
   const useDate = useDateStore((state) => state.date);
   const product = useProductFromUrl('product');
+  const [isSidebarVisible, setSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => setSidebarVisible(!isSidebarVisible);
 
   const getArgoData = useCallback(() => {
     const date = searchParams.get('date') || dayjs().format('YYYYMMDD');
@@ -64,18 +68,27 @@ const ProductLayout: React.FC = () => {
   }, [getArgoData, isArgo]);
 
   return (
-    <div className="mx-auto my-9 w-full max-w-8xl">
+    <div className="relative mx-auto my-9 w-full max-w-8xl">
       <div className="flex p-4">
-        <div className="w-1/3">
+        <button
+          onClick={toggleSidebar}
+          className="absolute -left-10 mb-4 flex h-28 items-center justify-center rounded bg-[#3A6F8F] px-4 py-2 text-white"
+        >
+          <img
+            src={ArrowIcon}
+            alt="Toggle Sidebar"
+            className={`h-5 w-5  transition-transform duration-300 ${isSidebarVisible ? 'rotate-90' : 'h-28 rotate-[270deg]'}`}
+          />
+        </button>
+        <div className={`transition-all duration-300 ${isSidebarVisible ? 'w-1/3' : 'w-0 overflow-hidden'}`}>
           <MapSidebar />
         </div>
-        <div className="ml-4 w-full">
+        <div className={`transition-all duration-300 ${isSidebarVisible ? 'ml-4' : 'ml-0'} w-full`}>
           <NewMapNavbar />
-          <>
-            <ErrorBoundary key={product?.mainProduct}>
-              <Outlet />
-            </ErrorBoundary>
-          </>
+
+          <ErrorBoundary key={product?.mainProduct}>
+            <Outlet />
+          </ErrorBoundary>
         </div>
       </div>
     </div>
