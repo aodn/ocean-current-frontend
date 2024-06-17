b8315321917e662cf0050ef5c2a05b6186a21f45
Weite Dai <weite.dai@utas.edu.au>
2024-06-17 14:38:39 +1000

    :white_check_mark: update failing tests in CI (#52)

--- src/components/ProductNavbar/ProductNavbar.test.tsx
+++ src/components/ProductNavbar/ProductNavbar.test.tsx
@@ -1,29 +1,67 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@/configs/dayjs';
+import useDateRange from '@/hooks/useDateRange/useDateRange';
 import ProductNavbar from './ProductNavbar';
 
+vi.mock('@/hooks/useDateRange/useDateRange');
+vi.mock('@/components/VideoCreation/VideoCreation', () => {
+  const MockedVideoCreation = () => <div>Download</div>;
+  return { default: MockedVideoCreation };
+});
+
 describe('ProductNavbar', () => {
+  const mockDates = [
+    new Date('2024-06-13'),
+    new Date('2024-06-14'),
+    new Date('2024-06-15'),
+    new Date('2024-06-16'),
+    new Date('2024-06-17'),
+  ];
+  const mockReturnValue = {
+    startDate: new Date('2024-06-13'),
+    endDate: new Date('2024-06-13'),
+    allDates: mockDates,
+    selectedDateIndex: 0,
+    handleSliderChange: vi.fn(),
+    handleDateChange: vi.fn(),
+    modifyDate: vi.fn(),
+    steps: 1,
+  };
+
   beforeEach(() => {
-    vi.useFakeTimers();
+    vi.mocked(useDateRange).mockReturnValue(mockReturnValue);
   });
 
   afterEach(() => {
-    vi.useRealTimers();
+    vi.restoreAllMocks();
   });
 
-  it('should render successfully', () => {
-    const date = new Date('13 June 2024');
-    vi.setSystemTime(date);
-
+  const renderComponentWithRouter = () => {
     render(
       <MemoryRouter>
         <ProductNavbar />
       </MemoryRouter>,
     );
+  };
+
+  it('should render successfully', () => {
+    renderComponentWithRouter();
 
     expect(screen.getByText('13 Jun 2024')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /right arrow icon/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /left arrow icon/i })).toBeInTheDocument();
+  });
+
+  it('should display video creation component', () => {
+    renderComponentWithRouter();
+
+    expect(screen.getByText('Download')).toBeInTheDocument();
+  });
+
+  it('should display slider component with the start date', () => {
+    renderComponentWithRouter();
+
+    expect(screen.getByText('13-06')).toBeInTheDocument();
   });
 });
