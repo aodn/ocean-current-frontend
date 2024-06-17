c29079f1932cb5a24cabc0814342b1b67099c389
Weite Dai <weite.dai@utas.edu.au>
2024-06-18 09:47:17 +1000

    :bug: clear timeout when component unmounted

--- src/components/DataVisualizationSidebar/DataVisualizationSidebar.tsx
+++ src/components/DataVisualizationSidebar/DataVisualizationSidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { TEXT_CONSTANT } from '@/constants/textConstant';
 import useProductCheck from '@/stores/product-store/hooks/useProductCheck';
 import ProductSideBar from './components/ProductSidebar';
@@ -7,16 +7,25 @@ import ArgoSideBar from './components/ArgoSideBar';
 const DataVisualizationSidebar: React.FC = () => {
   const [copyButtonText, setCopyButtonText] = useState<string>(TEXT_CONSTANT.COPY_PERMLINK);
   const { isArgo } = useProductCheck();
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleCopyLink = () => {
     const url = location.href;
     navigator.clipboard.writeText(url);
     setCopyButtonText('Copied!');
 
-    setTimeout(() => {
+    timeoutRef.current = setTimeout(() => {
       setCopyButtonText(TEXT_CONSTANT.COPY_PERMLINK);
     }, 2000);
   };
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
 
   return (
     <>
