439a7665e0b96def0e377903ef727c6a6ecfa7af
Weite Dai <weite.dai@utas.edu.au>
2024-06-21 16:32:13 +1000

    :sparkles: show map if no data in current region when change product (#61)

---
+++ src/stores/product-store/hooks/useProductAvailableInRegion.ts
@@ -0,0 +1,11 @@
+import { isProductAvailableInRegion } from '@/utils/region';
+import useProductStore from '../productStore';
+
+const useProductAvailableInRegion = () => {
+  const useProductId = useProductStore((state) => state.productParams.productId);
+  const useRegionTitle = useProductStore((state) => state.productParams.regionTitle);
+
+  return isProductAvailableInRegion(useRegionTitle, useProductId);
+};
+
+export default useProductAvailableInRegion;
