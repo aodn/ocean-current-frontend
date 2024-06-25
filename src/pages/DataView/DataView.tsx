08d7e9d6627866e139bec2cc33ffc045174ab98f
Weite Dai <weite.dai@utas.edu.au>
2024-06-25 13:36:28 +1000

    :bug: argo no need to check region availability

--- src/pages/DataView/DataView.tsx
+++ src/pages/DataView/DataView.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import useProductAvailableInRegion from '@/stores/product-store/hooks/useProductAvailableInRegion';
+import useProductCheck from '@/stores/product-store/hooks/useProductCheck';
 import MainMap from '../MapView/main-map/MainMap';
 import ProductContent from './product-content/ProductContent';
 
 const DataView: React.FC = () => {
+  const { isArgo } = useProductCheck();
   const isProductAvailableInRegion = useProductAvailableInRegion();
 
-  return isProductAvailableInRegion ? <ProductContent /> : <MainMap />;
+  const renderView = () => {
+    if (isArgo) {
+      return <ProductContent />;
+    }
+
+    return isProductAvailableInRegion ? <ProductContent /> : <MainMap />;
+  };
+
+  return renderView();
 };
 
 export default DataView;
