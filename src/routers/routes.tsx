4dee0f6bf7f7b6239c7f0fa6d8a7c199fe8bd8c4
Weite Dai <weite.dai@utas.edu.au>
2024-07-02 16:45:09 +1000

    :sparkles: fit map view when click region

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -2,7 +2,7 @@ import { Navigate, type RouteObject } from 'react-router-dom';
 import { Home, MapView, DataView, News, NotFound, GuidedTour } from '@/pages';
 import MainLayout from '@/layouts/LandingLayout';
 import MapLayout from '@/layouts/MapLayout';
-import ProductLayout from '@/layouts/DataVisualizationLayout';
+import DataVisualizationLayout from '@/layouts/DataVisualizationLayout';
 
 const routes: RouteObject[] = [
   {
@@ -15,7 +15,7 @@ const routes: RouteObject[] = [
       },
       {
         path: '/product',
-        element: <ProductLayout />,
+        element: <DataVisualizationLayout />,
         children: [
           {
             index: true,
