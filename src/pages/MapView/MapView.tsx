ec84b7028d8991f54c7b635a260e3ab20d91396d
Weite Dai <weite.dai@utas.edu.au>
2024-10-16 11:57:32 +1100

    :sparkles: adjust map zoom level for 4 hour sst (#121)

--- src/pages/MapView/MapView.tsx
+++ src/pages/MapView/MapView.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import MainMap from './main-map/MainMap';
+import React, { useEffect } from 'react';
+import BasicMap from '@/components/Map/BasicMap';
+import { resetMapStore } from '@/stores/map-store/mapStore';
 
 const MapView: React.FC = () => {
+  useEffect(() => {
+    resetMapStore();
+  }, []);
+
   return (
-    <div className="w-full">
-      <MainMap />
+    <div className="h-[660px] w-full">
+      <BasicMap />
     </div>
   );
 };
