528c9ee694b47e188fa2ae046eb9f263479e9932
kahmen <74268636+kahmencheang@users.noreply.github.com>
2024-11-27 14:52:20 +1100

    :lipstick: update minimap coordinates to two decimal places

--- src/components/Map/panels/MouseCursorLocationPanel/MouseCursorLocationPanel.tsx
+++ src/components/Map/panels/MouseCursorLocationPanel/MouseCursorLocationPanel.tsx
@@ -9,7 +9,7 @@ const MouseCursorLocationPanel: React.FC<MouseCursorLocationPanelProps> = ({ lat
   return (
     <div className="absolute bottom-0 mb-[10px] ml-[10px] w-36 rounded bg-white p-1 text-[14px] shadow-[0_0_0_2px_rgba(0,0,0,0.1)]">
       <div className="flex h-full w-full items-center justify-center bg-transparent">
-        {lat?.toFixed(4)} | {lng?.toFixed(4)}
+        {lat?.toFixed(2)} | {lng?.toFixed(2)}
       </div>
     </div>
   );
