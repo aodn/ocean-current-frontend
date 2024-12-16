2b6af5728f71b9e9f21db6d29bdadc390583ed16
kahmen <74268636+kahmencheang@users.noreply.github.com>
2024-12-16 17:25:43 +1100

    💄 update footer heading, text and UI

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import { copyrightInfo, footerAcknowledgeText, footerLinks, footerSocials } from './data/FooterData';
+import logo from '@/assets/images/imos-logo.png';
+import { GeneralText } from '@/constants/textConstant';
+import {
+  aodnInfo,
+  copyrightInfo,
+  footerAcknowledgeText,
+  footerLinks,
+  footerSocials,
+  ocSubheading,
+} from './data/FooterData';
 import { FooterIcon } from './types/footer.types';
-// import { FooterIcon, FooterLink } from './types/footer.types';
 
 const Footer: React.FC = () => {
   return (
-    <div className="flex w-full justify-center bg-white px-2">
+    <footer className="flex justify-center bg-white px-5 leading-8 sm:px-10">
       <div className="max-w-8xl py-6 md:pt-10">
-        <div className="text-2xl font-medium leading-7">IMOS OceanCurrent</div>
+        <div className="flex flex-row items-center py-5">
+          <img src={logo} alt="IMOS logo" className="h-8 sm:h-10 md:h-max" />
+          <div className="px-6 leading-6">
+            <h1 className="text-2xl font-medium text-imos-primary-1">{GeneralText.OC_PASCAL_CASE}</h1>
+            <h2 className="text-sm font-normal text-imos-subheading-grey">{ocSubheading}</h2>
+          </div>
+        </div>
 
-        <div className="flex flex-col justify-between border-y border-imos-primary-2 py-5 md:flex-row">
+        <p className="py-5">{aodnInfo}</p>
+
+        <div className="flex flex-col items-center gap-5 border-y border-imos-primary-2 py-5 sm:flex-row sm:justify-between">
           {footerLinks.map(({ text, url }) => (
             <a key={text} href={url} target="_blank" rel="noreferrer">
               {text}
@@ -18,18 +34,19 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="flex flex-col justify-between pt-5 md:flex-row">
-          <div className="md:w-1/2">{footerAcknowledgeText}</div>
-          <div className="flex gap-5 px-0.5 max-md:mt-10 md:self-end xl:self-end">
+          <p className="md:w-1/2">{footerAcknowledgeText}</p>
+          <div className="flex gap-5 self-center px-0.5 max-md:mt-10 md:self-end xl:self-end">
             {footerSocials.map(({ alt, src, url }: FooterIcon, index: number) => (
               <a key={index} href={url}>
-                <img loading="lazy" alt={alt} src={src} className="aspect-square w-5 shrink-0" />
+                <img loading="lazy" alt={alt} src={src} className="aspect-square w-6 shrink-0" />
               </a>
             ))}
           </div>
         </div>
-        <div className="mt-10 text-left text-sm leading-5">{copyrightInfo}</div>
+
+        <p className="mt-10 text-center text-sm leading-5 md:text-left">{copyrightInfo}</p>
       </div>
-    </div>
+    </footer>
   );
 };
 
