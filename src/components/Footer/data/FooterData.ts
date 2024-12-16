8d3289c2c5efa9f85e96eea19d5ec83595cbeafa
kahmen <74268636+kahmencheang@users.noreply.github.com>
2024-12-16 15:26:01 +1100

    💬 update static footer text constants

--- src/components/Footer/data/FooterData.ts
+++ src/components/Footer/data/FooterData.ts
@@ -1,4 +1,7 @@
-import { FooterLink } from '../types/footer.types';
+import facebookIcon from '@/assets/icons/facebook-icon.svg';
+import instagramIcon from '@/assets/icons/instagram-icon.svg';
+import linkedinIcon from '@/assets/icons/linkedin-icon.svg';
+import { FooterIcon, FooterLink } from '../types/footer.types';
 
 export const footerLinks: FooterLink[] = [
   {
@@ -14,3 +17,36 @@ export const footerLinks: FooterLink[] = [
     url: 'https://imos.org.au/resources/acknowledging-us',
   },
 ];
+
+export const footerAcknowledgeText: string =
+  'IMOS acknowledges the Traditional Custodians and Elders of the land and sea on which we work and observe and recognise their unique connection to land and sea. We pay our respects to Aboriginal and Torres Strait Islander peoples past and present.';
+
+export const footerSocials: FooterIcon[] = [
+  {
+    alt: 'IMOS Facebook',
+    src: facebookIcon,
+    url: 'https://www.facebook.com/IntegratedMarineObservingSystem',
+  },
+  {
+    alt: 'IMOS Instagram',
+    src: instagramIcon,
+    url: 'https://www.instagram.com/imos_australia/',
+  },
+  {
+    alt: 'IMOS X',
+    src: facebookIcon,
+    url: 'https://x.com/IMOS_AUS',
+  },
+  {
+    alt: 'IMOS Bluesky',
+    src: facebookIcon,
+    url: 'https://bsky.app/profile/imos-aus.bsky.social',
+  },
+  {
+    alt: 'IMOS LinkedIn',
+    src: linkedinIcon,
+    url: 'https://www.linkedin.com/company/imos_aus',
+  },
+];
+
+export const copyrightInfo: string = '© IMOS 2024';
