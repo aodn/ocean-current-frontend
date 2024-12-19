12adafacb9e9fb55d33b5af76d753201c804a23c
kahmen <74268636+kahmencheang@users.noreply.github.com>
2024-12-19 14:47:02 +1100

    🎨 remove home title component, update section UI

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { resetArgoStore } from '@/stores/argo-store/argoStore';
 import { resetMapStore } from '@/stores/map-store/mapStore';
+import { BrandingText } from '@/constants/textConstant';
 import HomeProductCarousel from './home-product-carousel/HomeProductCarousel';
-import HomeTitle from './home-title/HomeTitle';
 import HomeNews from './home-news/HomeNews';
 import HomeInWater from './home-in-water/HomeInWater';
 
@@ -13,22 +13,26 @@ const Home: React.FC = () => {
   }, []);
 
   return (
-    <>
-      <section className="mx-auto flex w-full max-w-8xl flex-col justify-center xl:mt-10 xl:flex-row">
-        <section>
-          <HomeTitle />
+    <section className="flex w-full flex-col bg-white">
+      <section className="flex w-full max-w-8xl flex-col justify-center self-center sm:px-2 md:px-4 xl:mt-10 xl:flex-row xl:pr-8">
+        <section className="my-8 flex flex-col items-center justify-center xl:my-0 xl:p-12">
+          <h1 className="lg:text-7xl font-lexend text-[42px] font-medium text-imos-dark-grey md:text-6xl">
+            {BrandingText.OC_PASCAL_CASE}
+          </h1>
+          <h2 className="lg:text-xl mt-2 px-4 text-center font-open-sans text-xs font-normal text-imos-text-grey sm:text-sm md:mt-4 md:text-lg xl:px-0">
+            {BrandingText.OC_SUBHEADING}
+          </h2>
         </section>
-        <div className="w-full md:w-1/2">
-          <HomeProductCarousel />
-        </div>
+
+        <HomeProductCarousel />
       </section>
-      <section className="mx-auto w-full max-w-8xl py-10">
+      <section className="w-full max-w-8xl self-center py-10">
         <HomeInWater />
       </section>
       <section className="bg-[#E5E8ED]">
         <HomeNews />
       </section>
-    </>
+    </section>
   );
 };
 
