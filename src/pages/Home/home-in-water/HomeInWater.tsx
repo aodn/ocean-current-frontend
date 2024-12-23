e9bb42b58fe0bc85e4ce0ac141c64fbdbe94f8d1
kahmen <74268636+kahmencheang@users.noreply.github.com>
2024-12-23 11:45:59 +1100

    ♻️ refactor component to use correct list of products

--- src/pages/Home/home-in-water/HomeInWater.tsx
+++ src/pages/Home/home-in-water/HomeInWater.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { homeInWaterData } from './data/homeInWaterData';
+import { linksData } from '@/data/linksData';
 
 const HomeInWater: React.FC = () => {
   return (
     <div className="p-6 text-center">
-      <h2 className="mb-10 text-center text-2xl text-imos-dark-grey">Explore More Ocean Data</h2>
       <div className="grid grid-cols-2 gap-6 md:grid-cols-7">
-        {homeInWaterData.map((item) => (
-          <Link
-            to={item.path}
-            key={item.name}
-            className="flex h-full cursor-pointer flex-col items-center justify-evenly rounded-lg bg-white p-4 shadow-md transition duration-300 ease-in-out hover:shadow-lg"
-          >
-            <img src={item.icon} alt={item.name} className="w-2h-24 mb-2 h-24" />
-            <span className="text-base text-imos-grey">{item.name}</span>
-          </Link>
+        {linksData.map((section) => (
+          <>
+            {section.links ? (
+              <>
+                {section.links.map((link) => (
+                  <div key={link.id} className="flex flex-col flex-wrap">
+                    <Link
+                      to={link.url}
+                      className="flex h-full cursor-pointer flex-col items-center justify-evenly rounded-lg bg-white p-4 shadow-md transition duration-300 ease-in-out hover:shadow-lg"
+                    >
+                      <img src={link.imageUrl} alt={link.description} className="w-2h-24 mb-2 h-24" />
+                      <span className="text-base text-imos-grey">{link.title}</span>
+                    </Link>
+                  </div>
+                ))}
+              </>
+            ) : (
+              <></>
+            )}
+          </>
         ))}
       </div>
     </div>
