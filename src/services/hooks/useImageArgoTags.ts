9f3d78b85c3d4e07bd8fbc8e8c775fe505370005
kahmen <74268636+kahmencheang@users.noreply.github.com>
2025-01-06 14:40:33 +1100

    Revert "✨  update argo tag region logic"
    This reverts commit d9d710acc3fe86edd42606fd7624ba0b3547eaca.

--- src/services/hooks/useImageArgoTags.ts
+++ src/services/hooks/useImageArgoTags.ts
@@ -5,20 +5,11 @@ import useDataFetch from './useDataFetch';
 
 const useImageArgoTags = (date: Dayjs, tagPath: string, regionCode: string) => {
   // SnapshotCHL is a special case on the server side
-  // EAC Mooring Array has data from only one region - Brisbane
-  const regionPath = (): string => {
-    switch (tagPath) {
-      case 'SnapshotCHL':
-        return `${regionCode}_chl`;
-      case 'EAC_array_figures':
-        return 'Brisbane';
-      default:
-        return regionCode;
-    }
-  };
+  const regionPath = tagPath === 'SnapshotCHL' ? `${regionCode}_chl` : regionCode;
 
-  const { data, loading, error } = useDataFetch(getArgoTags, [date, tagPath, regionPath()]);
+  const { data, loading, error } = useDataFetch(getArgoTags, [date, tagPath, regionPath]);
   const parsedData = data ? parseArgoTagDataFromText(data) : [];
+
   return { data: parsedData, loading, error };
 };
 
