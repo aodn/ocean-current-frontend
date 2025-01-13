83ae888a8a892db5e1757e26fdca3869635a8cad
kahmen <74268636+kahmencheang@users.noreply.github.com>
2025-01-13 15:03:43 +1100

    ♻️  update links data typing and navbar use

--- src/components/Navbar/components/NavbarMenu.tsx
+++ src/components/Navbar/components/NavbarMenu.tsx
@@ -4,9 +4,9 @@ import { DetailedLink } from '@/types/navbar';
 
 const NavbarMenu = forwardRef(({ items }: { items: DetailedLink[] }, ref: ForwardedRef<HTMLDivElement>) => {
   const renderMenuItems = (items: DetailedLink[]) =>
-    items.map(({ id, imageUrl, title, description, url }) => (
+    items.map(({ id, greyIcon, title, description, url }) => (
       <Link to={url} key={id} className={'flex justify-between gap-4 p-4 hover:bg-imos-light-blue hover:opacity-65'}>
-        <img alt={title} loading="lazy" src={imageUrl} className="my-auto aspect-square w-7" />
+        <img alt={title} loading="lazy" src={greyIcon} className="my-auto aspect-square w-7" />
         <div className="flex flex-1 flex-col whitespace-nowrap pr-1.5">
           <div className="text-base font-bold leading-5">{title}</div>
           <div className="text-sm font-light leading-5 text-gray-500">{description}</div>
