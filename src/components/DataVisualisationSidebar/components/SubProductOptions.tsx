7b7091120216bd5b084b83d7d945c058c5eec3a5
kahmen <74268636+kahmencheang@users.noreply.github.com>
2025-02-20 14:23:32 +1100

    ♻️  extract subproduct options to own component

---
+++ src/components/DataVisualisationSidebar/components/SubProductOptions.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Button } from '@/components/Shared';
+import { setProductId } from '@/stores/product-store/productStore';
+import { useQueryParams } from '@/hooks';
+import { ProductSidebarText } from '@/constants/textConstant';
+import {
+  CurrentMetersDepth,
+  CurrentMetersProperty,
+  CurrentMetersRegion,
+  mooredInstrumentArrayPath,
+} from '@/constants/currentMeters';
+import { setCurrentMetersDate, setDepth, setProperty, setRegion } from '@/stores/current-meters-store/currentMeters';
+import { yearOptionsData } from '@/data/current-meter/sidebarOptions';
+import { SubProductOptionsProps } from '../types';
+import CollapsibleSection from './CollapsibleSection';
+
+const SubProductOptions: React.FC<SubProductOptionsProps> = ({
+  isCurrentMeters,
+  subProducts,
+  subProductKey,
+  mainProductPath,
+}) => {
+  const { updateQueryParamsAndNavigate } = useQueryParams();
+
+  const handleSubProductChange = (key: string, subProductPath: string) => {
+    if (key === subProductKey) {
+      return;
+    }
+    setProductId(key);
+    const targetPath = `${mainProductPath}/${subProductPath}`;
+
+    let updateParam = {};
+    if (isCurrentMeters && subProductPath !== mooredInstrumentArrayPath) {
+      const allTime = yearOptionsData[0].id;
+
+      setRegion(CurrentMetersRegion.Aust);
+      setDepth(CurrentMetersDepth.ONE);
+      setProperty(CurrentMetersProperty.vrms);
+      setCurrentMetersDate(allTime);
+      updateParam = {
+        region: CurrentMetersRegion.Aust,
+        property: CurrentMetersProperty.vrms,
+        date: allTime,
+        depth: CurrentMetersDepth.ONE,
+      };
+    }
+    updateQueryParamsAndNavigate(targetPath, updateParam);
+  };
+
+  return (
+    <CollapsibleSection title={ProductSidebarText.OPTIONS}>
+      {subProducts.map(({ key, title, path }, index) => (
+        <div key={key}>
+          <Button
+            size={index === subProducts.length - 1 && subProducts.length % 2 !== 0 ? 'auto' : 'full'}
+            borderRadius="small"
+            type={key === subProductKey ? 'primary' : 'secondary'}
+            onClick={() => handleSubProductChange(key, path)}
+          >
+            <span className={`text-base ${key === subProductKey ? 'text-white' : 'text-imos-text-grey'}`}>{title}</span>
+          </Button>
+        </div>
+      ))}
+    </CollapsibleSection>
+  );
+};
+
+export default SubProductOptions;
--- src/components/DataVisualisationSidebar/types.ts
+++ src/components/DataVisualisationSidebar/types.ts
@@ -1,3 +1,6 @@
+import { ProductSidebarText } from '@/constants/textConstant';
+import { SubProduct } from '@/types/product';
+
 export type ProductInfo = {
   id: string;
   summary: string;
@@ -8,4 +11,16 @@ export type ProductInfo = {
 export interface ProductSummaryProp {
   isArgo: boolean;
   productInfo: ProductInfo;
+}
+
+export interface CollapsibleSectionProps {
+  title: ProductSidebarText;
+  children: JSX.Element | JSX.Element[];
+}
+
+export interface SubProductOptionsProps {
+  isCurrentMeters: boolean;
+  subProducts: SubProduct[];
+  subProductKey: string;
+  mainProductPath: string;
 }
