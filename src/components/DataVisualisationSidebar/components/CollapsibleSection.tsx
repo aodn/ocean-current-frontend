4a4f3dc33fffbf931bffd61672eb1be0321775ba
kahmen <74268636+kahmencheang@users.noreply.github.com>
2025-02-20 14:22:33 +1100

    ♻️ create sidebar collapsible section component

---
+++ src/components/DataVisualisationSidebar/components/CollapsibleSection.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import ArrowIcon from '@/assets/icons/arrow.svg';
+import { CollapsibleSectionProps } from '../types';
+
+const CollapsibleSection: React.FC<CollapsibleSectionProps> = ({ title, children }) => {
+  const [isSubProductsCollapsed, setIsSubProductsCollapsed] = useState(false);
+
+  return (
+    <div className="px-4">
+      <div
+        className="flex cursor-pointer items-center justify-between px-4 py-2"
+        onClick={() => setIsSubProductsCollapsed(!isSubProductsCollapsed)}
+        aria-hidden
+      >
+        <h3 className="text-lg font-medium text-imos-grey">{title}</h3>
+        <img
+          src={ArrowIcon}
+          alt="arrow icon"
+          className={`h-4 w-4 transform transition-transform duration-300 ${isSubProductsCollapsed ? 'rotate-180' : ''}`}
+        />
+      </div>
+      <div
+        className={`overflow-hidden transition-all duration-300 ${isSubProductsCollapsed ? 'max-h-0' : 'max-h-screen'}`}
+      >
+        <div className="mb-6 mt-2 flex flex-wrap justify-between gap-2">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CollapsibleSection;
