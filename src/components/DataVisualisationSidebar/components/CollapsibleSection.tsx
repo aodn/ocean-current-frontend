3eb0ea83ab4960050b3b4d6a6eaaa776ae0f589b
kahmen <74268636+kahmencheang@users.noreply.github.com>
2025-02-20 14:51:07 +1100

    ♻️  rename state

--- src/components/DataVisualisationSidebar/components/CollapsibleSection.tsx
+++ src/components/DataVisualisationSidebar/components/CollapsibleSection.tsx
@@ -3,25 +3,23 @@ import ArrowIcon from '@/assets/icons/arrow.svg';
 import { CollapsibleSectionProps } from '../types';
 
 const CollapsibleSection: React.FC<CollapsibleSectionProps> = ({ title, children }) => {
-  const [isSubProductsCollapsed, setIsSubProductsCollapsed] = useState(false);
+  const [isSectionCollapsed, setIsSectionCollapsed] = useState(false);
 
   return (
     <div className="px-4">
       <div
         className="flex cursor-pointer items-center justify-between px-4 py-2"
-        onClick={() => setIsSubProductsCollapsed(!isSubProductsCollapsed)}
+        onClick={() => setIsSectionCollapsed(!isSectionCollapsed)}
         aria-hidden
       >
         <h3 className="text-lg font-medium text-imos-grey">{title}</h3>
         <img
           src={ArrowIcon}
           alt="arrow icon"
-          className={`h-4 w-4 transform transition-transform duration-300 ${isSubProductsCollapsed ? 'rotate-180' : ''}`}
+          className={`h-4 w-4 transform transition-transform duration-300 ${isSectionCollapsed ? 'rotate-180' : ''}`}
         />
       </div>
-      <div
-        className={`overflow-hidden transition-all duration-300 ${isSubProductsCollapsed ? 'max-h-0' : 'max-h-screen'}`}
-      >
+      <div className={`overflow-hidden transition-all duration-300 ${isSectionCollapsed ? 'max-h-0' : 'max-h-screen'}`}>
         <div className="mb-6 mt-2 flex flex-wrap justify-between gap-2">{children}</div>
       </div>
     </div>
