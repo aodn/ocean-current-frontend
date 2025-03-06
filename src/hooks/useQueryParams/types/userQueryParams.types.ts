7446f5d529fd324f76f08ea59786f21126cc9ee3
kahmen <74268636+kahmencheang@users.noreply.github.com>
2025-03-06 13:05:48 +1100

    ✨ fix query param and type

--- src/hooks/useQueryParams/types/userQueryParams.types.ts
+++ src/hooks/useQueryParams/types/userQueryParams.types.ts
@@ -12,4 +12,5 @@ export type QueryParams = {
   depth?: string | null; // Current Meters, Argo
   wmoid?: string | null; // Argo
   cycle?: string | null; // Argo
+  point?: string | null; // Tidal Currents
 };
