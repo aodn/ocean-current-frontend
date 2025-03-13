469fd294528b192664ef68d902710d06202bf8db
kahmencheang <kahmen.dev@gmail.com>
2025-03-13 13:58:46 +1100

    ✅ extract util, add tests

--- src/services/tidalCurrents.ts
+++ src/services/tidalCurrents.ts
@@ -3,6 +3,26 @@ import { ContentType } from '@/constants/request';
 import { buildTidalCurrentsTagFileUrl } from '@/utils/data-image-builder-utils/dataImgBuilder';
 import { proxyClient } from './httpClient';
 
+const createTagsObjArray = (areaMatches: RegExpStringIterator<RegExpExecArray>) => {
+  return Array.from(areaMatches).map((match) => {
+    const attributes: Record<string, string | number[]> = {};
+    const attrMatches = match[1].matchAll(/(\w+)="([^"]+)"/g);
+
+    for (const attr of attrMatches) {
+      if (attr[1] !== 'target') {
+        const attrName = attr[1] === 'title' ? 'name' : attr[1];
+        attributes[attrName] =
+          attr[1] === 'coords'
+            ? attr[2].split(/\s+/).map(Number) // Convert coords to an array of numbers
+            : attr[2];
+      }
+    }
+
+    attributes['type'] = 'point';
+    return attributes;
+  });
+};
+
 const getTidalCurrentsTagsData = async (date: Dayjs, subProduct: string, region: string) => {
   if (!subProduct || !region || !date) {
     console.error('The subProduct, region and date are required to get Tidal Currents TAG file.');
@@ -20,27 +40,11 @@ const getTidalCurrentsTagsData = async (date: Dayjs, subProduct: string, region:
 
     const areaMatches = htmlString.data.matchAll(/<area\s+([^>]+)>/g);
 
-    return Array.from(areaMatches).map((match) => {
-      const attributes: Record<string, string | number[]> = {};
-      const attrMatches = match[1].matchAll(/(\w+)="([^"]+)"/g);
-
-      for (const attr of attrMatches) {
-        if (attr[1] !== 'target') {
-          const attrName = attr[1] === 'title' ? 'name' : attr[1];
-          attributes[attrName] =
-            attr[1] === 'coords'
-              ? attr[2].split(/\s+/).map(Number) // Convert coords to an array of numbers
-              : attr[2];
-        }
-      }
-
-      attributes['type'] = 'point';
-      return attributes;
-    });
+    return createTagsObjArray(areaMatches);
   } catch (error) {
     console.error('Error fetching and parsing Tidal Currents TAG data - ', error);
     throw new Error('Error fetching and parsing Tidal Currents TAG data');
   }
 };
 
-export { getTidalCurrentsTagsData };
+export { getTidalCurrentsTagsData, createTagsObjArray };
