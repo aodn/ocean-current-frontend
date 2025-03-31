cf845e60c65aaa87d1828d56fecf3919bf3f7d4a
kahmencheang <kahmen.dev@gmail.com>
2025-03-31 15:40:54 +1100

    Revert "Reapply "✨ add placeholder pop up for seal data""
    This reverts commit e00fb787229d04abc3af66d8d9ee557118bea11d.

--- src/pages/DataView/data-image/DataImageWithSealCtdGraphs.tsx
+++ src/pages/DataView/data-image/DataImageWithSealCtdGraphs.tsx
@@ -7,7 +7,7 @@ import { MapImageAreas } from '@/types/dataImage';
 import { buildSealCtdGraphImageUrl } from '@/utils/data-image-builder-utils/dataImgBuilder';
 import { getSealCtdGraphTags, validateSealCtdImgUrl } from '@/services/sealCtd';
 import { imageBaseUrl } from '@/configs/image';
-import { Loading, Popup } from '@/components/Shared';
+import { Loading } from '@/components/Shared';
 import { parseSealCtdTagData } from '@/utils/seal-ctd-utils/sealStdTags';
 
 type DataImageWithSealCtdGraphsProps = {
@@ -46,8 +46,6 @@ const DataImageWithSealCtdGraphs: React.FC<DataImageWithSealCtdGraphsProps> = ({
   const [imgUrls, setImgUrls] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [hasImgLoaded, setHasImgLoaded] = useState<boolean>(false);
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [selectedSealTag, setSelectedSealTag] = useState<string>('');
 
   // generate and validate image urls
   useEffect(() => {
@@ -100,19 +98,6 @@ const DataImageWithSealCtdGraphs: React.FC<DataImageWithSealCtdGraphsProps> = ({
     return <Loading />;
   }
 
-  const openPopUp = (sealTag: string) => {
-    setSelectedSealTag(sealTag);
-    setIsPopupOpen(true);
-  };
-  const closePopUp = () => {
-    setSelectedSealTag('');
-    setIsPopupOpen(false);
-  };
-
-  const PopupBody = () => {
-    return <div className="p-4">{selectedSealTag} selected</div>;
-  };
-
   const handleImageLoad = () => {
     if (imgRef.current) {
       const { naturalWidth: originalWidth, naturalHeight: originalHeight, width, height } = imgRef.current;
@@ -160,11 +145,10 @@ const DataImageWithSealCtdGraphs: React.FC<DataImageWithSealCtdGraphsProps> = ({
                       shape={area.shape}
                       coords={area.coords.join(',')}
                       alt={`${altText} for ${area.alt}`}
-                      onClick={() => openPopUp(area.name)}
+                      onClick={() => {}}
                       onKeyDown={(e) => {
                         if (e.key === 'Enter' || e.key === ' ') {
                           e.preventDefault();
-                          openPopUp(area.name);
                         }
                       }}
                       tabIndex={0}
@@ -177,7 +161,6 @@ const DataImageWithSealCtdGraphs: React.FC<DataImageWithSealCtdGraphsProps> = ({
           </>
         );
       })}
-      <Popup title="sattags data" body={PopupBody} isOpen={isPopupOpen} onClose={closePopUp} />
     </div>
   );
 };
