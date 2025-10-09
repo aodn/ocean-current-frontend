466ca74c2375b8d5664cb4f6ab5ed1bb71e56980
Zhonghui Duan <Zhonghui.Duan@utas.edu.au>
2025-10-09 15:49:55 +1100

    :bug: make useArgoData safe

--- src/components/Map/hooks/useArgoData.ts
+++ src/components/Map/hooks/useArgoData.ts
@@ -5,13 +5,25 @@ import { DateFormat } from '@/types/date';
 import { useCurrentPage } from '@/hooks/useCurrentPage/useCurrentPage';
 import useArgoDataQuery from './useArgoDataQuery';
 
+/**
+ * Hook to fetch Argo data based on the current page context.
+ *
+ * On the product page, it fetches Argo data for the user-selected date.
+ * On other pages (map page), it fetches Argo data for the latest available date.
+ *
+ * @returns {Object} An object containing:
+ *   - argoData: GeoJSON FeatureCollection of Argo profiles
+ *   - error: Error object if the query failed, undefined otherwise
+ */
 const useArgoData = () => {
-  const useDate = useDateStore((state) => state.date);
-  const latestArgoDateString = useRegionLatestDates('argo').data?.regionLatestDates?.[0]?.latestDate;
-  const latestArgoDate = dayjs(latestArgoDateString, DateFormat.DAY);
-
   const currentPage = useCurrentPage();
   const isProductPage = currentPage === '/product';
+
+  const useDate = useDateStore((state) => state.date);
+
+  const { data: latestDatesData } = useRegionLatestDates('argo', !isProductPage);
+  const latestArgoDateString = latestDatesData?.regionLatestDates?.[0]?.latestDate;
+  const latestArgoDate = latestArgoDateString ? dayjs(latestArgoDateString, DateFormat.DAY) : dayjs('invalid');
 
   const { argoData: argoDataProductPage, error: productPageError } = useArgoDataQuery({
     enabled: isProductPage && useDate.isValid(),
