b48efec6bdc262fe84a83d3191d3c78d68be2388
Weite Dai <weite.dai@utas.edu.au>
2025-10-21 14:11:06 +1100

    :lipstick: update carousel text colour

--- src/pages/Home/home-map-carousel/HomeMapCarousel.tsx
+++ src/pages/Home/home-map-carousel/HomeMapCarousel.tsx
@@ -71,8 +71,8 @@ const HomeMapCarousel: React.FC = () => {
 
       <div className="flex flex-col rounded-b-xl border border-solid border-imos-calypso-blue border-opacity-60">
         <div className="flex min-h-36 flex-col px-10 py-10 md:py-6">
-          <h2 className="pb-2 font-poppins text-lg font-semibold text-imos-dark-grey">{selectedProduct.title}</h2>
-          <p className="imos-dark-grey font-open-sans text-base">{selectedProduct.description}</p>
+          <h2 className="pb-2 font-poppins text-lg font-semibold text-imos-nav-text">{selectedProduct.title}</h2>
+          <p className="font-open-sans text-base text-imos-dark-grey">{selectedProduct.description}</p>
         </div>
 
         <div className="mb-10 flex justify-center gap-2">
