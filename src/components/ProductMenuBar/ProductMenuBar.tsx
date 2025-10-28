ab78d8e62a55ef5a86c046053ad7061016c9d3be
Weite Dai <weite.dai@utas.edu.au>
2025-10-28 11:22:26 +1100

    :iphone: remove radius on mobile version

--- src/components/ProductMenuBar/ProductMenuBar.tsx
+++ src/components/ProductMenuBar/ProductMenuBar.tsx
@@ -143,7 +143,7 @@ const ProductMenuBar: React.FC<ProductMenuBarProps> = ({ setShowVideo, isMapView
   ]);
 
   return (
-    <div className="mb-2 w-full rounded-b-md rounded-t-none bg-white p-2 md:rounded-md md:bg-transparent md:p-0">
+    <div className="mb-2 w-full bg-white p-2 md:rounded-md md:bg-transparent md:p-0">
       <div className="flex w-full flex-wrap items-center gap-2 font-sans font-medium text-imos-dark-grey md:mb-2 md:gap-3">
         <div className="flex h-11 grow basis-[calc(100%-4rem)] items-center justify-between rounded-md border border-imos-calypso-blue/50 bg-white md:grow md:basis-auto md:border-none">
           {isCurrentMeters ? (
