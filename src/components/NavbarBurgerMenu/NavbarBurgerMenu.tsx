b2c7f6b6da69fabee6606324d05b5b58a3748c2b
Zhonghui Duan <Zhonghui.Duan@utas.edu.au>
2025-11-04 15:58:28 +1100

    :bug: update margin top under navbar for mobie

--- src/components/NavbarBurgerMenu/NavbarBurgerMenu.tsx
+++ src/components/NavbarBurgerMenu/NavbarBurgerMenu.tsx
@@ -38,64 +38,62 @@ const NavbarBurgerMenu = ({ closeMobileMenu }: { closeMobileMenu: () => void }) 
   };
 
   return (
-    <div>
-      <nav className="mt-20 w-full bg-transparent px-4">
-        <div className="overflow-hidden rounded-lg bg-imos-cloud-tint shadow-sm">
-          {linksData.map((item, index) => (
-            <div key={item.title}>
-              <CollapsibleComponent
-                defaultOpen={index === 0}
-                disable={!item.links || item.links.length === 0}
-                toggleIconHidden={!item.links || item.links.length === 0}
-                wrapperClassName={`w-full border-imos-light-grey ${index === linksData.length - 1 ? '' : 'border-b-2'}`}
-                trigger={({ toggle, open, direction, toggleIconHidden }: TriggerArgs) => (
-                  <CollapsibleTrigger
-                    open={open}
-                    toggle={toggle}
-                    direction={direction}
-                    toggleIconHidden={toggleIconHidden}
-                    label={
-                      item.url ? (
-                        <LinkOrAnchor
-                          className="mb-4 text-base font-medium text-gray-900"
-                          to={item.url}
-                          onClick={closeMobileMenu}
+    <nav className="mt-20 w-full bg-transparent px-4">
+      <div className="overflow-hidden rounded-lg bg-imos-cloud-tint shadow-sm">
+        {linksData.map((item, index) => (
+          <div key={item.title}>
+            <CollapsibleComponent
+              defaultOpen={index === 0}
+              disable={!item.links || item.links.length === 0}
+              toggleIconHidden={!item.links || item.links.length === 0}
+              wrapperClassName={`w-full border-imos-light-grey ${index === linksData.length - 1 ? '' : 'border-b-2'}`}
+              trigger={({ toggle, open, direction, toggleIconHidden }: TriggerArgs) => (
+                <CollapsibleTrigger
+                  open={open}
+                  toggle={toggle}
+                  direction={direction}
+                  toggleIconHidden={toggleIconHidden}
+                  label={
+                    item.url ? (
+                      <LinkOrAnchor
+                        className="mb-4 text-base font-medium text-gray-900"
+                        to={item.url}
+                        onClick={closeMobileMenu}
+                      >
+                        {item.title}
+                      </LinkOrAnchor>
+                    ) : (
+                      <span className="mb-4 text-base font-medium text-gray-900">{item.title}</span>
+                    )
+                  }
+                />
+              )}
+            >
+              {item.links && (
+                <div className="my-[1px]">
+                  {item.links?.length > 0 &&
+                    item.links.map(({ id, url, Icon, title }) => (
+                      <LinkOrAnchor
+                        key={id}
+                        className="mr-auto block w-full font-semibold text-gray-700"
+                        to={url}
+                        onClick={closeMobileMenu}
+                      >
+                        <p
+                          className={`flex w-full items-center gap-x-4 p-3 ${isLinkActive(url) ? 'bg-imos-light-blue' : ''}`}
                         >
-                          {item.title}
-                        </LinkOrAnchor>
-                      ) : (
-                        <span className="mb-4 text-base font-medium text-gray-900">{item.title}</span>
-                      )
-                    }
-                  />
-                )}
-              >
-                {item.links && (
-                  <div className="my-[1px]">
-                    {item.links?.length > 0 &&
-                      item.links.map(({ id, url, Icon, title }) => (
-                        <LinkOrAnchor
-                          key={id}
-                          className="mr-auto block w-full font-semibold text-gray-700"
-                          to={url}
-                          onClick={closeMobileMenu}
-                        >
-                          <p
-                            className={`flex w-full items-center gap-x-4 p-3 ${isLinkActive(url) ? 'bg-imos-light-blue' : ''}`}
-                          >
-                            <Icon size="xxl" color="imos-grey" />
-                            {title}
-                          </p>
-                        </LinkOrAnchor>
-                      ))}
-                  </div>
-                )}
-              </CollapsibleComponent>
-            </div>
-          ))}
-        </div>
-      </nav>
-    </div>
+                          <Icon size="xxl" color="imos-grey" />
+                          {title}
+                        </p>
+                      </LinkOrAnchor>
+                    ))}
+                </div>
+              )}
+            </CollapsibleComponent>
+          </div>
+        ))}
+      </div>
+    </nav>
   );
 };
 
